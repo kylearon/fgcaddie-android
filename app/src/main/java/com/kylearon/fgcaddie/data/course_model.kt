fb0b19badf2c1bd4cbc13ab123ba23479969f78b
Kyle Aron <aron.kyle@gmail.com>
2023-04-14 02:11:03 -0400

    Make some parts of the data model optional.

--- app/src/main/java/com/kylearon/fgcaddie/data/course_model.kt
+++ app/src/main/java/com/kylearon/fgcaddie/data/course_model.kt
@@ -16,9 +16,9 @@ data class Courses(
 @Serializable
 data class Course(
     val guid: String,
-    val name: String,
-    val creator: String,
-    val holes: MutableList<Hole>
+    val name: String = "default",
+    val creator: String = "default",
+    val holes: MutableList<Hole> = ArrayList()
 )
 
 @Serializable
@@ -28,9 +28,9 @@ data class Hole(
     val hole_number: Int,
     var par: Int,
     var length: Int,
-    var shots_tee: MutableList<Shot>,
-    var shots_approach: MutableList<Shot>,
-    var shots_putt: MutableList<Shot>
+    var shots_tee: MutableList<Shot> = ArrayList(),
+    var shots_approach: MutableList<Shot> = ArrayList(),
+    var shots_putt: MutableList<Shot> = ArrayList()
 )
 
 @Serializable
